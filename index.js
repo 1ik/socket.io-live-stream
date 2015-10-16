792071c8447b37cdb828b300d474f131526eb77a
anik <=>
2015-10-16 21:47:54 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/client', function(req, res){
+  res.sendFile(__dirname + '/client.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+  socket.on('video', function(frame){
+  	io.emit('client', frame); //send frame to all clients.
+  });
+
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
